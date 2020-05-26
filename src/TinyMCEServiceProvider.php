fa89819d5837ba640a4c5ee20e061ba870a2a44d
tabuna <bliz48rus@gmail.com>
2020-05-27 01:19:30 +0300

    Initial commit

---
+++ src/TinyMCEServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tabuna\TinyMCE;
+
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+use Orchid\Platform\Dashboard;
+use Orchid\Platform\Providers\EventServiceProvider;
+use Orchid\Platform\Providers\FoundationServiceProvider;
+use Orchid\Platform\Providers\PlatformServiceProvider;
+
+class TinyMCEServiceProvider extends RouteServiceProvider
+{
+
+    /**
+     * @param Dashboard $dashboard
+     */
+    public function boot(): void
+    {
+        $this->loadViewsFrom(__DIR__ . '/../views', 'tinymce');
+
+        \Orchid\Support\Facades\Dashboard::addPublicDirectory('tinymce', __DIR__ . '/../resource');
+    }
+}
---
+++ views/field.blade.php
@@ -0,0 +1,8 @@
+@component($typeForm, get_defined_vars())
+    <div data-controller="tinymce" data-theme="{{$theme ?? 'inlite'}}">
+        <div class="tinymce border p-3" id="tinymce-wrapper-{{$id}}" style="min-height: {{ $attributes['height'] }}">
+            {!! $value !!}
+        </div>
+        <input type="hidden" @attributes($attributes)>
+    </div>
+@endcomponent
